d7c8174bea50c0ea071824dd5cdf5399fd3485c0
CRGMBC <carolinergrant@hotmail.com>
2023-08-06 15:03:21 +1000

    simple data analysis completed

---
+++ EmployeeSQL/2. employees hired in 1986.sql
@@ -0,0 +1,3 @@
+SELECT first_name, last_name, hire_date
+from employees
+WHERE hire_date >= '1986-01-01' AND hire_date <= '1986-12-31';
---
+++ EmployeeSQL/8. frequeny_of_last-name.sql
@@ -0,0 +1,3 @@
+select last_name as "Last name", count(emp_no) as "Number of employees" 
+from employees
+group by last_name;
